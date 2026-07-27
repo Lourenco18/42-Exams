f0512f5e3e0e653e191a1a50931f8f551e640821
Lourenco18 <contact.lourenco18@gmail.com>
2026-07-27 14:25:28 +0100

    array_rotation DONE

--- exam_rank04/exam/Level 1/py_array_rotation_detector.py
+++ exam_rank04/exam/Level 1/py_array_rotation_detector.py
@@ -14,3 +14,30 @@ array_rotation_detector([1, 2, 3], [3, 2, 1])->False
 array_rotation_detector([1, 2], [1, 2, 3])->False
 array_rotation_detector([], [])->True"""
 
+def twister(nums, n) -> list:
+    if len(nums) == 0:
+        return []
+    n = n % len(nums)
+    return nums[-n:]+ nums[:-n]
+
+def array_rotation_detector(arr1: list, arr2: list) -> bool:
+    if len(arr1) == 0:
+        if len(arr2) == 0:
+            return True
+    if len(arr1) != len(arr2):
+        return False
+    i = 0
+    while i < len(arr1):
+        if(twister(arr1,i) == arr2):
+            return True
+        i= i+1
+
+    return False
+
+print(array_rotation_detector([1, 2, 3, 4, 5], [4, 5, 1, 2, 3]))
+print(array_rotation_detector([1, 2, 3, 4, 5], [5, 1, 2, 3, 4]))
+print(array_rotation_detector([1, 2, 3], [3, 2, 1]))
+print(array_rotation_detector([1, 2], [1, 2, 3]))
+print(array_rotation_detector([1, 2], []))
+print(array_rotation_detector([], []))
+
