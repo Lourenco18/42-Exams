62b63b862025c185c16f3edeeda1f08198b90d38
Lourenco18 <contact.lourenco18@gmail.com>
2026-07-28 21:10:47 +0100

    constellation mapper done

--- exam_rank04/exam/Level 1/py_constellation_mapper.py
+++ exam_rank04/exam/Level 1/py_constellation_mapper.py
@@ -12,3 +12,24 @@ constellation_mapper([(0, 0), (0, 1), (0, 2), (1, 1), (2, 2)], 3)->["***", ".*."
 constellation_mapper([(0, 0), (5, 5), (2, 2)], 3)->["*..", "...", "..*"]
 constellation_mapper([(0, 0), (5, 5)], 2)->["*.", ".."]
 """
+def constellation_mapper(stars: list[tuple[int, int]], size: int) -> list[str]:
+    grid = []
+    i = 0
+    """criar linha"""
+    while i < size:
+        grid.append(["."] * size)
+        i += 1
+    for row, col in stars:
+        if 0 <= row < size and 0 <= col < size:
+            grid[row][col] = "*"
+    result = []
+    for row in grid:
+        result.append("".join(row))
+
+    return result
+
+print(constellation_mapper([(0, 0), (1, 1), (2, 2)], 3))
+print(constellation_mapper([(0, 0), (0, 1), (0, 2), (1, 1), (2, 2)], 3))
+print(constellation_mapper([(0, 0), (5, 5), (2, 2)], 3))
+print(constellation_mapper([(0, 0), (5, 5)], 2))
+print(constellation_mapper([(0, 0), (0, 0), (0, 0)], 2))
