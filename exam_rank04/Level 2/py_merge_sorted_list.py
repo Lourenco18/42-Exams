07245db0a869a873c7a8d8d55b71ec28f6ebfd14
Lourenco18 <contact.lourenco18@gmail.com>
2026-07-29 13:19:15 +0100

    merge sorted lists done

--- exam_rank04/Level 2/py_merge_sorted_list.py
+++ exam_rank04/Level 2/py_merge_sorted_list.py
@@ -10,8 +10,21 @@ Constraints:
 FUNCTION SIGNATURE
 def merge_sorted_list(lists: list[list[int]]) ->[ list[int]:
 EXAMPLES
-merge_sorted_list([[1, 4, 5], [1, 3, 4], [2, 6]])->[1, 1, 2, 3, 4, 4, 5, 6]
+merge_sorted_list([[1, , 5], [1, 3, 4], [2, 6]])->[1, 1, 2, 3, 4, 4, 5, 6]
 merge_sorted_list([[1, 2, 3], [], [0, 4]])->[0, 1, 2, 3, 4]
 merge_sorted_list([])->[]
 merge_sorted_list([[], []])->[]
 """
+def merge_sorted_list(lists: list[list[int]]) -> list[int]:
+    if not lists:
+        return []  
+    i = 0
+    result = []
+    while i < len(lists):
+        result+=  sorted(lists[i])
+        i+=1
+    return sorted(result)
+print(merge_sorted_list([[1, 4, 5], [1, 3, 4], [2, 6]]))
+print(merge_sorted_list([[1, 2, 3], [], [0, 4]]))
+print(merge_sorted_list([]))
+print(merge_sorted_list([[], []]))
